901d8facd132ac2424ff4090275437e7b3ef8929
Paulo Cesar C Bruno <p_bruno001@hotmail.com>
2022-04-04 04:51:41 -0300

    Refatorado

--- Program.cs
+++ Program.cs
@@ -19,12 +19,14 @@ builder.Services.AddCors();
 
 var app = builder.Build();
 
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    app.UseBrowserLink();// Colocar Aqui se não estiver em desenvolvimento -> if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
@@ -33,7 +35,6 @@ app.UseCors(c => c.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod());
 
 
 app.UseRouting();
-
 app.UseAuthorization();
 
 app.MapControllerRoute(
